7465e2b7f17907e4136ed184727263dcd5a3a49d
Daniel P <sakinad0514@gmail.com>
2025-02-10 10:56:59 -0500

    subiendo el snipe-it

---
+++ config/version.php
@@ -0,0 +1,10 @@
+<?php
+return array (
+  'app_version' => 'v7.1.16',
+  'full_app_version' => 'v7.1.16 - build 16564-gfb857ccf5',
+  'build_version' => '16564',
+  'prerelease_version' => '',
+  'hash_version' => 'gfb857ccf5',
+  'full_hash' => 'v7.1.16-510-gfb857ccf5',
+  'branch' => 'master',
+);
